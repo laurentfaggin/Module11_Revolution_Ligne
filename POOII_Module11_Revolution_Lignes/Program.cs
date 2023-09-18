076f367ce185383b7f5a22fde54efeddfd5dadfd
laurentfaggin <laurentfaggin@hotmail.fr>
2023-09-18 08:28:07 -0400

    mise en place structure

--- POOII_Module11_Revolution_Lignes/Program.cs
+++ POOII_Module11_Revolution_Lignes/Program.cs
@@ -11,7 +11,7 @@ namespace POOII_Module11_Revolution_Lignes
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new fRevolutionLignes());
         }
     }
 }
---
+++ POOII_Module11_Revolution_Lignes/fRevolutionLignes.cs
@@ -0,0 +1,15 @@
+namespace POOII_Module11_Revolution_Lignes
+{
+    public partial class fRevolutionLignes : Form
+    {
+        public fRevolutionLignes()
+        {
+            InitializeComponent();
+        }
+
+        private void lbAngle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
